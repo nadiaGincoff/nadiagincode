d66ac6ec5c40f8d10d8fccd4363f08832225efc8
nadiaGincoff <nadia.gincoff@gmail.com>
2024-07-13 21:41:35 +0200

    icon size modified

--- src/components/SlidingImages/index.tsx
+++ src/components/SlidingImages/index.tsx
@@ -13,38 +13,38 @@ import { SiGit } from "react-icons/si";
 const slider1 = [
     {
         color: "#e1dad6",
-        icon: <FaNode className='sm:text-5xl md:text-[10rem] lg:text-[20rem]' color="var(--darkgray)" />
+        icon: <FaNode className='sm:text-5xl md:text-[7rem] lg:text-[15rem]' color="var(--darkgray)" />
     },
     {
         color: "#d6d7dc",
-        icon: <RiReactjsFill className='sm:text-5xl md:text-[10rem] lg:text-[20rem]' color="var(--secondary)" />
+        icon: <RiReactjsFill className='sm:text-5xl md:text-[7rem] lg:text-[15rem]' color="var(--secondary)" />
     },
     {
         color: "var(--beige)",
-        icon: <SiTypescript className='sm:text-5xl md:text-[10rem] lg:text-[20rem]' color="var(--darkgreen)" />
+        icon: <SiTypescript className='sm:text-5xl md:text-[7rem] lg:text-[15rem]' color="var(--darkgreen)" />
     },
     {
         color: "var(--beige)",
-        icon: <SiGit className='sm:text-5xl md:text-[10rem] lg:text-[20rem]' color="var(--secondary)" />
+        icon: <SiGit className='sm:text-5xl md:text-[7rem] lg:text-[15rem]' color="var(--secondary)" />
     }
 ]
 
 const slider2 = [
     {
         color: "var(--color-bg2)",
-        icon: <SiTailwindcss className='sm:text-5xl md:text-[10rem] lg:text-[20rem]' color="var(--secondary)" />
+        icon: <SiTailwindcss className='sm:text-5xl md:text-[7rem] lg:text-[15rem]' color="var(--secondary)" />
     },
     {       
         color: "#e5e0e1",
-        icon: <SiMysql className='sm:text-5xl md:text-[10rem] lg:text-[20rem]' color="var(--secondary)" />
+        icon: <SiMysql className='sm:text-5xl md:text-[7rem] lg:text-[15rem]' color="var(--secondary)" />
     },
     {
         color: "var(--darkgray)",
-        icon: <SiFirebase className='sm:text-5xl md:text-[10rem] lg:text-[20rem]' color="var(--beige)" />
+        icon: <SiFirebase className='sm:text-5xl md:text-[7rem] lg:text-[15rem]' color="var(--beige)" />
     },
     {
         color: "#e1dad6",
-        icon: <SiMongodb className='sm:text-5xl md:text-[10rem] lg:text-[20rem]' color="var(--darkgreen)" />
+        icon: <SiMongodb className='sm:text-5xl md:text-[7rem] lg:text-[15rem]' color="var(--darkgreen)" />
     },
     
 ]
